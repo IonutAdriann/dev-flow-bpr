be254e8af76673d75586311d9dc4ba4cd09bd642
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2023-12-07 14:11:05 +0100

    fix: fix bugs and errors
    Co-Authored-By: Andronache George Eduard <42812475+neutralfusion@users.noreply.github.com>

--- middleware.ts
+++ middleware.ts
@@ -4,17 +4,16 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhook",
-    "/question/:id",
+    "question/:id",
     "/tags",
     "/tags/:id",
     "/profile/:id",
     "/community",
     "/jobs",
-    "/ask-question",
   ],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
-  matcher: "/((?!.*\\..*|_next|api).*)", // Simplified matcher
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
