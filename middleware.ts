04bb77b4716066db9161323870f073507140e335
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2023-12-08 17:24:27 +0100

    feat: add all components
    Co-Authored-By: Andronache George Eduard <42812475+neutralfusion@users.noreply.github.com>

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,11 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhook",
-    "question/:id",
+    "/question/:id",
     "/tags",
     "/tags/:id",
     "/profile/:id",
     "/community",
-    "/jobs",
   ],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
